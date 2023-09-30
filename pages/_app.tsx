6e00a2af8569ec96b2788aa7dab93d958ba7b2fc
Shubham Saxena <91315996+shubham-saxena-pdgt@users.noreply.github.com>
2023-09-30 16:54:31 +0530

    chore: set up base

---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { ChakraProvider } from '@chakra-ui/react';
+
+import '@/styles/globals.css';
+import { theme } from '@/styles/theme';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Head>
+        <title>Next</title>
+      </Head>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+}
---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+import { Box } from '@chakra-ui/react';
+
+export default function NewsListPage() {
+  return (
+    <Box bg="white">Hello</Box>
+  );
+}
